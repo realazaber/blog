37fba49cf5df5f712ab840289edd3f4546e3cf67
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-02-06 03:08:52 +1100

    index created

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <center>
+        <h1>
+            Blog
+        </h1>
+        <h3>
+            Welcome to Blog
+        </h3>
+        <p>
+            Blog is a free and open source platform where you can create blogs with 
+            text, images and videos and have people interact with it.
+        </p>
+        <p>
+            This site is constantly in development and change notes can be found <i>here</i>.
+        </p>
+        <p>
+            Happy blogging!
+        </p>
+    </center>
+
+    <footer>
+        Version: 0.1
+    </footer>
+</body>
+</html>
