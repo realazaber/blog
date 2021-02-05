88e013ffe4fa3ff0f61e7c1073d0ef2eb891bf4a
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-02-06 03:47:48 +1100

    more addtions
    nav, basic styling

--- index.php
+++ index.php
@@ -3,30 +3,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stylesheets/main.css" rel="stylesheet">
+    <link href="stylesheets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="stylesheets/bootstrap/js/bootstrap.min.js"></script>
     <title>Blog</title>
 </head>
 <body>
     <center>
-        <h1>
-            Blog
-        </h1>
-        <h3>
-            Welcome to Blog
-        </h3>
-        <p>
-            Blog is a free and open source platform where you can create blogs with 
-            text, images and videos and have people interact with it.
-        </p>
-        <p>
-            This site is constantly in development and change notes can be found <i>here</i>.
-        </p>
-        <p>
-            Happy blogging!
-        </p>
+        <?php
+            include("includes/nav.inc");
+        ?>
+        <main>
+            <h3>
+                Welcome to Blog
+            </h3>
+            <p>
+                Blog is a free and open source platform where you can create blogs with 
+                text, images and videos and have people interact with it.
+            </p>
+            <p>
+                This site is constantly in development and change notes can be found <i>here</i>.
+            </p>
+            <p>
+                Happy blogging!
+            </p>
+        </main>
+
     </center>
 
-    <footer>
-        Version: 0.1
-    </footer>
+    <?php
+        include("includes/version.inc");
+    ?>
 </body>
 </html>
